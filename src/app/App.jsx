93e7d3beec47896bf7899bc3fab863af169f1ef0
Hiruni Prarthana <137154585+Hiruni29145@users.noreply.github.com>
2025-08-28 10:44:40 +0530

    add businesspage feature

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Dashboard from "../page/Dashboard/Dashboard";
 import Home from "../page/Home/Home";
 import UserPage from "../page/UserPage/UserPage";
-// import Login from "../page/Login/Login"; // optional
+import Business from "../page/Business/Business"; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Optional login route */}
-        {/* <Route path="/login" element={<Login />} /> */}
-
-        {/* Dashboard layout with nested pages */}
         <Route path="/" element={<Dashboard />}>
           <Route index element={<Navigate to="home" replace />} />
           <Route path="home" element={<Home />} />
           <Route path="userpage" element={<UserPage />} />
+          <Route path="businesses" element={<Business />} /> 
           <Route path="*" element={<Navigate to="home" replace />} />
         </Route>
       </Routes>
